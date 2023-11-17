de41f6611c8ceaafe6efa5d46f185b8808279dc1
marinaesca <marinaescalante@utexas.edu>
2023-11-17 14:32:33 -0700

    exercise 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import '../box-styles.css'
+import PropTypes from 'prop-types';
 
 // 🐨 add a className prop to each div and apply the correct class names
 // based on the text content
@@ -14,16 +15,118 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className={`box box--small`}
+//     style={{ backgroundColor: 'lightblue', fontStyle: 'italic' }}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = <div
+//   className={`box box--medium`}
+//   style={{ backgroundColor: 'pink', fontStyle: 'italic'}}
+// >
+//   medium pink box
+// </div>
+// const largeBox = <div
+//   className={`box box--large`}
+//   style={{ backgroundColor: 'orange', fontStyle: 'italic'}}
+// >
+//   large orange box
+// </div>
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+
+// function Box({className = '', style, text, ...otherProps}) {
+//   return (
+//     <div
+//       className={`box ${className}`}
+//       style={{fontStyle: 'italic', ...style}}
+//       {...otherProps}
+//       >
+//       {text}
+//     </div>
+//   )
+// }
+
+// const smallBox = (
+//   <Box
+//     className='box--small'
+//     style={{backgroundColor: 'lightblue'}}
+//     text='small lightblue box'
+//   />
+// );
+// const mediumBox = (
+//   <Box
+//     className='box--medium'
+//     style={{backgroundColor: 'pink'}}
+//     text='medium pink box'
+//   />
+// );
+// const largeBox = (
+//   <Box
+//     className='box--large'
+//     style={{backgroundColor: 'orange'}}
+//     text='large orange box'
+//   />
+// );
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+function Box({size, style, text, ...otherProps}) {
+  return (
+    <div
+      className={`box box--${size}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+      >
+      {text}
+    </div>
+  )
+}
+
+Box.propTypes = {
+  size: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  text: PropTypes.string,
+};
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box
+        size='small'
+        style={{backgroundColor: 'lightblue'}}
+        text='small lightblue box'
+      />
+      <Box
+        size='medium'
+        style={{backgroundColor: 'pink'}}
+        text='medium pink box'
+      />
+      <Box
+        size='large'
+        style={{backgroundColor: 'orange'}}
+        text='large orange box'
+      />
     </div>
   )
 }
